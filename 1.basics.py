6fdaaf9f7a2626c10f3bc36a41ec2004b9df24c3
Krish-1018 <k72908519@gmail.com>
2025-09-15 18:02:56 +0530

    Add files via upload

---
+++ 1.basics.py
@@ -0,0 +1,37 @@
+# for i in range (100):
+#     print("I am sorry babu")
+
+#1-5 print
+# for i in range(1,6):
+#     print(i)
+
+#5-1 print
+# for i in range(5,0,-1):
+#     print(i)
+
+#print, count and sum all even number from 1-100
+
+
+# for i in range(1,101,1):
+#     if i%2==0:
+#        print(i)
+
+# for i in range(2,101,2):
+#     print(i)
+
+# for i in range(1,101,1):
+#     i%2==0 and print(i) #short circuit
+
+# count = 0
+# sum = 0
+# for i in range(1,101):
+#     if i%2==0 :
+#         print(i)
+#         count += 1
+#         sum += i
+# print(count) 
+# print(sum)  
+
+
+for i in range(-5,-1):
+    print(i)
